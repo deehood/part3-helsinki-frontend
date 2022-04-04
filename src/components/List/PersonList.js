7306cd36f428ce7298fa5e4869fe6cf5e00efd95
deehood <dee.hood@gmail.com>
2022-04-04 12:36:56 +0100

    changed a list key on Personlist component and read.me

--- src/components/List/PersonList.js
+++ src/components/List/PersonList.js
@@ -1,29 +1,29 @@
 import React from "react";
 import PersonLine from "./PersonLine";
 const PersonList = ({
-  persons,
-  filter,
-  handleDelete,
-  handleNotification,
-  refreshPersons,
+    persons,
+    filter,
+    handleDelete,
+    handleNotification,
+    refreshPersons,
 }) => {
-  return (
-    <>
-      {persons
-        .filter((person) =>
-          person.name.toLowerCase().includes(filter.toLowerCase())
-        )
-        .map((person) => (
-          <PersonLine
-            key={persons.id}
-            person={person}
-            handleDelete={handleDelete}
-            handleNotification={handleNotification}
-            refreshPersons={refreshPersons}
-          />
-        ))}
-    </>
-  );
+    return (
+        <>
+            {persons
+                .filter((person) =>
+                    person.name.toLowerCase().includes(filter.toLowerCase())
+                )
+                .map((person) => (
+                    <PersonLine
+                        key={person.id}
+                        person={person}
+                        handleDelete={handleDelete}
+                        handleNotification={handleNotification}
+                        refreshPersons={refreshPersons}
+                    />
+                ))}
+        </>
+    );
 };
 
 export default PersonList;
